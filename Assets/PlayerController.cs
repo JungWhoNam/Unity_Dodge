f90407ade78dce6e2d2587dc76c3b23e6466555e
Jung Who Nam <jungwhonam@gmail.com>
2025-03-30 19:15:11 +0900

    Add HW6

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,26 +12,23 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        // 수평과 수직 축 입력 값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        // 실제 이동 속도를 입력 값과 이동 속력을 통해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vector3 속도를 (xSpeed, 0, zSpeed)으로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        // 리지드바디의 속도에 newVelocity를 할당
+        playerRigidbody.linearVelocity = newVelocity;
     }
 
     public void Die()
     {
+        // 자신의 게임 오브젝트를 비활성화
         gameObject.SetActive(false);
     }
 }
