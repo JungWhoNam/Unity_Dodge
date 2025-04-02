d4d6d9843059e7d5c0c073ed75e9df9b61e8c9f9
Jung Who Nam <jungwhonam@gmail.com>
2025-04-02 21:16:33 +0900

    Update HW6 sol

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,18 +16,14 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
-        // 수평 또는 수직 축 입력 값이 있을 때만 linearVelocity를 설정
-        if (xInput != 0 || zInput != 0)
-        {
-            // 실제 이동 속도를 입력 값과 이동 속력을 통해 결정
-            float xSpeed = xInput * speed;
-            float zSpeed = zInput * speed;
+        // 실제 이동 속도를 입력 값과 이동 속력을 통해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-            // Vector3 속도를 (xSpeed, 0, zSpeed)으로 생성
-            Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
-            // 리지드바디의 속도에 newVelocity를 할당
-            playerRigidbody.linearVelocity = newVelocity;
-        }
+        // Vector3 속도를 (xSpeed, 기존 y 속도, zSpeed)으로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, playerRigidbody.linearVelocity.y, zSpeed);
+        // 리지드바디의 속도에 newVelocity를 할당
+        playerRigidbody.linearVelocity = newVelocity;
     }
 
     public void Die()
