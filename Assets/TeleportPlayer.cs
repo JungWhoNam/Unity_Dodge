ccebe346de584ae7020e29e98077290bdf570bc1
Jung Who Nam <jungwhonam@gmail.com>
2025-04-06 15:59:22 +0900

    Add bullet spawner

--- Assets/TeleportPlayer.cs
+++ Assets/TeleportPlayer.cs
@@ -5,11 +5,9 @@ public class TeleportPlayer : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             other.gameObject.transform.position = new Vector3(0, 1, 0);
         }
     }
-
-
 }
